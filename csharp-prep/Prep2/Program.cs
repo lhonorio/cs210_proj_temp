e80eda2cc00da5d15ee1db0cf69a9af63a04c32d
Luiz Honorio <lhonorio@byui.edu>
2023-04-22 16:12:31 -0600

    Done Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string userInput = Console.ReadLine();
+        int gradePct = int.Parse(userInput);
+
+        string letter = "";
+
+        if (gradePct >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePct >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePct >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePct >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}.");
+
+        if (gradePct >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("I am sorry, you didn't pass. But don't give up and try again next semester.");
+        }
     }
 }
