82522fd9900c7cc61a17711d60cfbb2ad4aeb59b
Luiz Honorio <lhonorio@byui.edu>
2023-06-13 08:58:00 -0600

    Finished Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        Square square = new Square("blue", 3);
+        Rectangle rect = new Rectangle("red", 3, 4);
+        Circle circ = new Circle("green", 2);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rect);
+        shapes.Add(circ);
+        
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}\nArea: {shape.GetArea()}");
+        }
+
+        // Console.WriteLine($"Color: {square.GetColor()}\nArea: {square.GetArea()}");
+        // Console.WriteLine($"Color: {rect.GetColor()}\nArea: {rect.GetArea()}");
+        // Console.WriteLine($"Color: {circ.GetColor()}\nArea: {circ.GetArea()}");
     }
 }
